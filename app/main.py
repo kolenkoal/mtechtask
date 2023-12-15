655d14a16e8928a0552eeaa9f1489240336d3c54
kolenkoal <kolenko.alexander0@gmail.com>
2023-12-15 19:22:10 +0300

    Created PostgreSQL DB

--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+# Подключение к базе данных
+POSTGRESQL_DATABASE = "postgresql://user:password@db:5432/mvideotest"
+engine = create_engine(POSTGRESQL_DATABASE)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
