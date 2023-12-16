9d03bb3ca67848e0a26402815aa9d67bb8acdf1b
kolenkoal <kolenko.alexander0@gmail.com>
2023-12-16 14:17:40 +0300

    Fixed client service

--- client/main.py
+++ client/main.py
@@ -5,14 +5,17 @@ import time
 import random
 
 
-def send_request(ip, http_method, uri, http_response):
+def send_request(ip, http_method, uri, http_response, log_file):
     log_text = f"{ip} {http_method} {uri} {http_response}"
 
     payload = {"log": log_text}
     headers = {"Content-Type": "application/json"}
 
-    response = requests.post('http://web:8000/api/data/', json=payload,
-                             headers=headers)
+    requests.post('http://web:8000/api/data/', json=payload,
+                  headers=headers)
+
+    with open(log_file, 'a') as file:
+        file.write(log_text + '\n')
 
 
 def random_delay(max_delay):
@@ -21,15 +24,16 @@ def random_delay(max_delay):
 
 num_threads = int(os.environ.get('NUMBER_OF_REQUESTS', 5))
 max_delay = int(os.environ.get('MAX_DELAY_IN_MS', 1000))
+log_file_path = 'logging.txt'
 
 while True:
     for _ in range(num_threads):
         ip_address = '192.168.1.1'
-        http_method = random.choice(["GET", "POST", "DEELTE", "PUT"])
+        http_method = random.choice(["GET", "POST", "DELETE", "PUT"])
         uri = '/example'
         http_response = random.randint(200, 500)
 
         thread = threading.Thread(target=send_request, args=(
-            ip_address, http_method, uri, http_response))
+            ip_address, http_method, uri, http_response, log_file_path))
         thread.start()
         time.sleep(random_delay(max_delay))
