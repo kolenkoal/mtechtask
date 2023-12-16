aa6e00c5b82e1a107bf491c825f5d2964e83d637
kolenkoal <kolenko.alexander0@gmail.com>
2023-12-16 14:04:53 +0300

    Added client service

--- client/main.py
+++ client/main.py
@@ -1,42 +1,35 @@
 import os
-import asyncio
+import requests
+import threading
+import time
 import random
-from fastapi import FastAPI
-import aiohttp
-
-app = FastAPI()
-
-N = int(os.getenv("NUM_REQUESTS", 5))
-M = int(os.getenv("MAX_DELAY", 1000))
 
 
-async def send_requests():
-    ip_address = "0.0.0.0"
-    http_method = "POST"
-    url = "http://web:8000/api/data/"
-    uri = '/api/data'
+def send_request(ip, http_method, uri, http_response):
+    log_text = f"{ip} {http_method} {uri} {http_response}"
 
-    async with aiohttp.ClientSession() as session:
-        for _ in range(N):
-            http_response = random.randint(200, 500)
+    payload = {"log": log_text}
+    headers = {"Content-Type": "application/json"}
 
-            async with session.post(url, json={
-                "log": f"{ip_address} {http_method} {uri} {http_response}"}) as response:
-                log_text = f"{ip_address} {http_method} {uri} {http_response}\n"
-
-                # print(response)
-                json_data = await response.json()
-
-                print(json_data)
-
-                with open("request_logs.txt", "a") as log_file:
-                    log_file.write(log_text)
-
-                await asyncio.sleep(random.uniform(0, M) / 1000)
+    response = requests.post('http://web:8000/api/data/', json=payload,
+                             headers=headers)
 
 
-@app.get("/simulate_requests/")
-async def simulate_requests():
-    tasks = [send_requests() for _ in range(N)]
-    await asyncio.gather(*tasks)
-    return {"message": f"{N} requests simulated"}
+def random_delay(max_delay):
+    return random.uniform(0, max_delay) / 1000.0
+
+
+num_threads = int(os.environ.get('NUMBER_OF_REQUESTS', 5))
+max_delay = int(os.environ.get('MAX_DELAY_IN_MS', 1000))
+
+while True:
+    for _ in range(num_threads):
+        ip_address = '192.168.1.1'
+        http_method = random.choice(["GET", "POST", "DEELTE", "PUT"])
+        uri = '/example'
+        http_response = random.randint(200, 500)
+
+        thread = threading.Thread(target=send_request, args=(
+            ip_address, http_method, uri, http_response))
+        thread.start()
+        time.sleep(random_delay(max_delay))
