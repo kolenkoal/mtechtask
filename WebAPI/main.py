656486860dc6c2454701843c67058a72fb09fdbe
kolenkoal <kolenko.alexander0@gmail.com>
2023-12-16 21:28:00 +0300

    Fixed output of docker container

--- WebAPI/main.py
+++ WebAPI/main.py
@@ -3,4 +3,8 @@ from core.endpoints.endpoints import router
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup_event():
+    print("INFO:     Uvicorn running on http://127.0.0.1:8000/api/data/ (Press CTRL+C to quit)")
+
 app.include_router(router)
