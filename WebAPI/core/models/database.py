2125943c09e7296b6cff6dcd5d4c5da91fe927a3
kolenkoal <kolenko.alexander0@gmail.com>
2023-12-16 08:54:17 +0300

    Changed project structure

---
+++ WebAPI/core/models/database.py
@@ -0,0 +1,28 @@
+import psycopg2
+
+
+def get_db_connection():
+    """
+    Настроить соединение с базой данных.
+    :return:
+    """
+    return psycopg2.connect(
+        dbname="mvideotest",
+        user="user",
+        password="password",
+        host="db"
+    )
+
+
+def execute_query(query, params=None):
+    """
+    Запустить запрос для взаимодействия с базой данных.
+    :param query:
+    :param params:
+    :return:
+    """
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(query, params)
+    conn.commit()
+    return cursor
